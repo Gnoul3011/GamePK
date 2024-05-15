dd956250acb954c170e96a9b492f7ae04e6a8c85
kimada2002 <quangnhatdz008@gmail.com>
2024-05-15 19:49:17 +0700

    reset cac man choi

--- fighter.py
+++ fighter.py
@@ -176,3 +176,34 @@ class Fighter():
             #update the animation from django.conf import settings
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
+            
+    def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False) #player1 quay mat qua trai
+        # pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
+    
+    def reset(self, player, x, y, flip, data, sprite_sheet, animation_steps, sound):
+        self.player = player
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.flip = flip
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0 #0:idle #1:run #2:jump #3:attack1 #4: attack2 #5:hit #6:death
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time = pygame.time.get_ticks()
+        self.rect = pygame.Rect((x, y, 80, 180))
+        self.vel_y = 0
+        self.running = False
+        self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.attack_cooldown = 0
+        self.attack_sound = sound
+        self.hit = False
+        self.health = 100
+        self.mana = 0
+        self.max_mana = 100
+        self.alive = True
+        
