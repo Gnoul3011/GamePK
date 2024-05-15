4ebad8b49af1fca9738247a9fbeaf1e0360a2c30
kimada2002 <quangnhatdz008@gmail.com>
2024-05-15 19:41:30 +0700

    nhan vat

--- main.py
+++ main.py
@@ -8,3 +8,50 @@ pygame.init()
 # create game window
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("Game Multiplayer")
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define colours
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+# Biến đếm và cập nhật countdown
+intro_count = 3 
+last_count_update = pygame.time.get_ticks()
+
+# Điểm số và các biến liên quan
+score = [0, 0] 
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
+
+#define fighter variables
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [76,56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
+
+# Load âm nhạc và âm thanh
+pygame.mixer.music.load("assets/audio/music1.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1, 0.0, 5000)
+sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
+sword_fx.set_volume(0.5)
+magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
+magic_fx.set_volume(0.75)
+KO_fx = pygame.mixer.Sound("assets/audio/ko.mp3")
+KO_fx.set_volume(0.5)
+P1_win_fx = pygame.mixer.Sound("assets/audio/player-1-wins.mp3")
+P1_win_fx.set_volume(0.25)
+P2_win_fx = pygame.mixer.Sound("assets/audio/player-2-wins.mp3")
+P2_win_fx.set_volume(0.25)
