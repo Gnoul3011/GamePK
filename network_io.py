6af568b7fc8b4c12e0ce61053796133b7f097749
Nhat1207 <minhnhatnguyenphan1207@gmail.com>
2024-05-16 14:29:16 +0700

    [IO]complete kingdom.io

--- network_io.py
+++ network_io.py
@@ -3,7 +3,7 @@ import pickle as pickle
 
 
 class Network_IO:
-    def init(self):
+    def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #self.client.settimeout(10.0)
         self.host = "192.168.1.228"
