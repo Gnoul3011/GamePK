f11a9e8fb7f6d8d8eb95c33ba22f64876f4cbf24
kimada2002 <quangnhatdz008@gmail.com>
2024-05-16 10:26:44 +0700

    create network_online.py

---
+++ network_online.py
@@ -0,0 +1,25 @@
+import socket
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.1.45"
+        self.port = 5557
+        self.addr = (self.server, self.port)
+        self.data = self.connect()
+        
+    def getData(self):
+        return self.data
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
